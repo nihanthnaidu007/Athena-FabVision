89a33a5c09fb7a1f0ccd87e31907e94200532e36
Kalisetti Nihanth Naidu <kalisettinihanthnaidu@Kalisettis-MacBook-M2.local>
2024-11-16 01:50:12 +0530

    Add root URL and fix favicon issue

--- django_agent/urls.py
+++ django_agent/urls.py
@@ -1,24 +1,13 @@
-"""
-URL configuration for django_agent project.
+from django.views.static import serve
+from django.conf import settings
+from django.urls import re_path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+def home_view(request):
+    return HttpResponse('<h1>Welcome to Athena AI Agent</h1><p>Go to <a href="/agent/">Agent</a> to interact with the AI.</p>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agent/', include('agent.urls')), 
+    path('agent/', include('agent.urls')),
+    path('', home_view, name='home'),
+    re_path(r'^favicon\.ico$', serve, {'path': 'favicon.ico', 'document_root': settings.STATIC_ROOT}),
 ]
-
